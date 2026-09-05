61c994bb7c0e9a215d6a47f766cc0b02f46b0bc5
mixa3607 <30209772+mixa3607@users.noreply.github.com>
2026-09-06 02:11:59 +0500

    delete mcodes command

--- src/ArkProjects.UefiModTools/Commands/UefiTools/CommandRegistration.cs
+++ src/ArkProjects.UefiModTools/Commands/UefiTools/CommandRegistration.cs
@@ -10,7 +10,6 @@ public class CommandRegistration
         var command = new Command("uefi", "UEFI related tools");
         parentCommand.Add(command);
 
-        Microcodes.CommandRegistration.Register(command, services);
         Fit.FitCommandRegistration.Register(command, services);
         SetupData.SetupDataCommandRegistration.Register(command, services);
         Sct.SctCommandRegistration.Register(command, services);
--- src/ArkProjects.UefiModTools/Commands/UefiTools/Microcodes/CommandJsonSerializerContextMicrocodes.cs
+++
@@ -1,8 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace ArkProjects.UefiModTools.Commands.UefiTools.Microcodes;
-
-[JsonSerializable(typeof(MicrocodesTable))]
-internal partial class CommandJsonSerializerContextMicrocodes : JsonSerializerContext
-{
-}
